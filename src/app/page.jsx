86f9eabde57ab04383d3bf6e79bbac2e7b14ca04
Foxy20122012 <enriquemerlos49@gmail.com>
2023-01-30 23:19:27 -0600

    primer commit

---
+++ src/app/page.jsx
@@ -0,0 +1,19 @@
+import '../styles/globals.css'
+import Tablero from '../components/Tablejo'
+
+
+ const Home=()=> {
+  return (
+    <div className='bg-green-300 '>
+    <div className='mx-auto container flex justify-center'>
+      <Tablero className="m-5 p-2"/>
+    </div>
+       
+   
+    </div>
+      
+  )
+}
+
+
+export default Home
---
+++ src/components/Tablejo.jsx
@@ -0,0 +1,25 @@
+
+
+const Tablero =()=>{
+    return (
+        <div className="">
+            <div className='grid grid-cols-3 gap-1'>
+                            
+                <div className='bg-gradient-to-r from-violet-500 to-fuchsia-500 h-28 w-28 border-2 border-amber-400  text-center rounded-lg'>1</div>
+                <div className='bg-gradient-to-r from-violet-500 to-fuchsia-500 h-28 w-28 border-2 border-amber-400 text-center rounded-lg'>2</div>
+                <div className='bg-gradient-to-r from-violet-500 to-fuchsia-500 h-28 w-28 border-2 border-amber-400 text-center rounded-lg'>3</div>
+
+                <div className='bg-gradient-to-r from-violet-500 to-fuchsia-500 h-28 w-28 border-2 border-amber-400 text-center rounded-lg'>4</div>
+                <div className='bg-gradient-to-r from-violet-500 to-fuchsia-500 h-28 w-28 border-2 border-amber-400 text-center rounded-lg'>5</div>
+                <div className='bg-gradient-to-r from-violet-500 to-fuchsia-500 h-28 w-28 border-2 border-amber-400 text-center rounded-lg'>6</div>
+
+                <div className='bg-gradient-to-r from-violet-500 to-fuchsia-500 h-28 w-28 border-2 border-amber-400 text-center rounded-lg'>7</div>
+                <div className='bg-gradient-to-r from-violet-500 to-fuchsia-500 h-28 w-28 border-2 border-amber-400 text-center rounded-lg'>8</div>
+                <div className='bg-gradient-to-r from-violet-500 to-fuchsia-500 h-28 w-28 border-2 border-amber-400 text-center rounded-lg'>9</div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Tablero
