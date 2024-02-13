775258aca52af01f37e1164d96a4c998ee5a6818
Foxy20122012 <malvarezm26@miumg.edu.gt>
2024-02-12 20:55:09 -0600

    Funcion para casilla ocupada

--- src/components/Tablejo.jsx
+++ src/components/Tablejo.jsx
@@ -1,67 +1,66 @@
-'use client';
-import { useContext } from "react";
-import {useState} from "react"
-import Cuadro from "./cuadro"
+'use client'
+import { useState } from "react";
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Cuadro from "./cuadro";
 
+const Tablero = () => {
+    const [cuadros, setCuadros] = useState(Array(9).fill(' '));
+    const [turno, setTurno] = useState('X');
 
-
-const Tablero =()=>{
-    const[cuadros, setCuadros] = useState(Array(9).fill(' '))
-    const [turno, setTurno] = useState('X')
-
-
-
-    const pintaFigura = (indexItem) =>{
-        const misCuadritos =cuadros.slice()
-        misCuadritos.splice(indexItem, 1, turno)
-        setCuadros(misCuadritos)
-        if (turno === 'X'){
-            setTurno('O')
-            } else{
-                setTurno("X")
+    const pintaFigura = (indexItem) => {
+        if (cuadros[indexItem] === ' ') {
+            const misCuadritos = cuadros.slice();
+            misCuadritos.splice(indexItem, 1, turno);
+            setCuadros(misCuadritos);
+            if (turno === 'X') {
+                setTurno('O');
+            } else {
+                setTurno('X');
+            }
+        } else {
+            // Mostrar toast indicando que el cuadro ya está ocupado
+            toast.error("Este cuadro ya está ocupado.", {
+                position: "top-center"
+            });
         }
     }
 
-    const calculaGanador =(mytablero)=>{
-        const jugadasGanadoras =[
-            [0,1,2],
-            [3,4,5],
-            [6,7,8],
-            [0,3,6],
-            [1,4,7],
-            [2,5,7],
-            [0,4,8],
-            [2,4,6],
+    const calculaGanador = (mytablero) => {
+        const jugadasGanadoras = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 7],
+            [0, 4, 8],
+            [2, 4, 6],
         ];
-        for (let indicejugada = 0; indicejugada < jugadasGanadoras.length; indicejugada++){
-            const[a, b, c]= jugadasGanadoras[indicejugada];
-        if (mytablero[a] && mytablero[a] === mytablero[b] && mytablero[a] === mytablero[c]){
-            return mytablero[a];
-        }
+        for (let indicejugada = 0; indicejugada < jugadasGanadoras.length; indicejugada++) {
+            const [a, b, c] = jugadasGanadoras[indicejugada];
+            if (mytablero[a] && mytablero[a] === mytablero[b] && mytablero[a] === mytablero[c]) {
+                return mytablero[a];
+            }
         }
         return '';
     }
 
     return (
         <div className="">
-            <span>{'El ganador es: '+ calculaGanador(cuadros)}</span>
+            <span>{'El ganador es: ' + calculaGanador(cuadros)}</span>
             <div className='grid grid-cols-3 gap-1'>
-                            
-              {
-                cuadros.map((item, indexItem)=>{
-                   
-
+                {cuadros.map((item, indexItem) => {
                     return <Cuadro
-                     key={indexItem} 
-                     valor={item}
-                     alHacerClick ={()=> pintaFigura(indexItem)}
-                     />
-                })
-              }
-
+                        key={indexItem}
+                        valor={item}
+                        alHacerClick={() => pintaFigura(indexItem)}
+                    />
+                })}
             </div>
+            <ToastContainer />
         </div>
     )
 }
 
-export default Tablero
+export default Tablero;
